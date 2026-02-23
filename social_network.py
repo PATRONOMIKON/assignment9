86ed0d3c0e5821493bd1034b522a122151f107db
Bohdan Olefirenko <bohdan.olefirenko2k4@gmail.com>
2026-02-22 20:42:46 -0500

    Debugged final output issues

--- social_network.py
+++ social_network.py
@@ -8,8 +8,14 @@ class Person:
         add_friend(friend): Adds a friend to the person's friend list.
    '''
     
-    pass # Delete this line to implement the Person class
+def __init__(self, name):
+        self.name = name
+        self.friends = []
 
+def add_friend(self, friend):
+        """Add a friend if not already connected."""
+        if friend not in self.friends:
+            self.friends.append(friend)        
 
 class SocialNetwork:
     '''
@@ -22,8 +28,64 @@ class SocialNetwork:
         print_network(): Prints the names of all people and their friends.
     '''
     
-    pass # Delete this line to implement the SocialNetwork class
+def __init__(self):
+        self.people = {}
 
+def add_person(self, name):
+        """Add a new person to the network."""
+        if name in self.people:
+            print(f"{name} already exists in the network.")
+        else:
+            self.people[name] = Person(name)
 
+def add_friendship(self, person1_name, person2_name):
+        """Create bidirectional friendship."""
+        if person1_name not in self.people:
+            print(f"Friendship not created. {person1_name} doesn't exist!")
+            return
+        if person2_name not in self.people:
+            print(f"Friendship not created. {person2_name} doesn't exist!")
+            return
+
+        person1 = self.people[person1_name]
+        person2 = self.people[person2_name]
+
+        person1.add_friend(person2)
+        person2.add_friend(person1)
+
+def print_network(self):
+        """Print each person and their friends."""
+        for name, person in self.people.items():
+            friend_names = [friend.name for friend in person.friends]
+            friends_str = ", ".join(friend_names)
+            print(f"{name} is friends with: {friends_str}")            
 
 # Test your code here
+
+if __name__ == "__main__":
+    network = SocialNetwork()
+
+    # Add people
+    network.add_person("Alex")
+    network.add_person("Jordan")
+    network.add_person("Morgan")
+    network.add_person("Taylor")
+    network.add_person("Casey")
+    network.add_person("Riley")
+
+    # Edge case: duplicate
+    network.add_person("Alex")
+
+    # Create friendships (8+)
+    network.add_friendship("Alex", "Jordan")
+    network.add_friendship("Alex", "Morgan")
+    network.add_friendship("Jordan", "Taylor")
+    network.add_friendship("Jordan", "Johnny")  # error case
+    network.add_friendship("Morgan", "Casey")
+    network.add_friendship("Taylor", "Riley")
+    network.add_friendship("Casey", "Riley")
+    network.add_friendship("Morgan", "Riley")
+    network.add_friendship("Alex", "Taylor")
+
+    print()
+    network.print_network()
